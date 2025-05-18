70b0d6ed3f30112d559c8cc035417a43713ec63e
amanpandey13 <u.getaman1303@gmail.com>
2025-05-18 13:48:31 +0530

    update app.py and add profile photo also add projects

--- app.py
+++ app.py
@@ -1,6 +1,23 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, send_from_directory
+import os
 
 app = Flask(__name__)
+
+@app.after_request
+def add_header(response):
+    response.headers['Cache-Control'] = 'no-store, no-cache, must-revalidate, max-age=0'
+    response.headers['Pragma'] = 'no-cache'
+    response.headers['Expires'] = '0'
+    return response
+
+# Override static file behavior to prevent 304
+@app.route('/static/<path:filename>')
+def custom_static(filename):
+    return send_from_directory(
+        os.path.join(app.root_path, 'static'),
+        filename,
+        conditional=False  # <- disables 304
+    )
 
 @app.route('/')
 def home():
